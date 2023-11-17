abbb4cec10861b62a619890c1a00d4d1e25e4ab9
ddkktt <128318872+ddkktt@users.noreply.github.com>
2023-11-17 12:55:28 -0600

    login form

--- tunehub/src/app/pages/login/login.component.ts
+++ tunehub/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../user.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder, 
+    private userService: UserService
+    ) {
+    this.loginForm = formBuilder.group({
+      username: ['', Validators.required, Validators],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  onSubmit() {
+    console.log('object');
+    const userData = {
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password,
+    };
+    this.userService.login(userData).subscribe({}
+    )
+  }
 
 }
