cc5f0c7f46b833b58ba0369410c377dd0168290a
ddkktt <128318872+ddkktt@users.noreply.github.com>
2023-11-22 11:30:51 -0600

    artist

--- tunehub/src/app/pages/profile/profile.component.ts
+++ tunehub/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
+import { NgTemplateOutlet } from '@angular/common';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  user: null = null;
+
+  constructor(private userService: UserService){}
+  
+  ngOnInit(): void{
+    this.loadUser();
+  }
+
+  loadUser(): void{
+
+
+  }
+  
+
+
+
+
 
 }
