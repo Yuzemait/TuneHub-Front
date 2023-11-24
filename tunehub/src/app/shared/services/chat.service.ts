2f54c539182cdc8464d3110a9dfd2de765170ddb
ddkktt <128318872+ddkktt@users.noreply.github.com>
2023-11-24 07:54:23 -0600

    .

--- tunehub/src/app/shared/services/chat.service.ts
+++ tunehub/src/app/shared/services/chat.service.ts
@@ -11,6 +11,15 @@ export class ChatService {
 
   constructor(private httpClient: HttpClient) { }
 
+  joinChat(chat_id:string, user_id:string) {
+    if(chat_id && user_id){
+      const url: string = `${environment.apiUrl}users/user-data`;
+      return this.httpClient.get(url);
+    }
+    return 'not found'
+  }
+
+
 
   getUserChatsbyChatId(chat_id:string):Observable<Chat> {
     const url: string = `${environment.apiUrl}chats/${chat_id}`
