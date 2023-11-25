85a8731693da236c9ce4008feec482abcf7c32a4
Yuzemait <jose.andres15@hotmail.com>
2023-11-24 20:37:47 -0600

    song.service

--- tunehub/src/app/pages/profile/create-song/create-song.component.ts
+++ tunehub/src/app/pages/profile/create-song/create-song.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { SongService } from 'src/app/shared/services/song.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-song',
@@ -7,11 +9,48 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./create-song.component.scss']
 })
 export class CreateSongComponent {
-  constructor(private dialogRef: MatDialogRef<CreateSongComponent>) {}
+  constructor(private dialogRef: MatDialogRef<CreateSongComponent>, private formBuilder: FormBuilder, private songService: SongService) {
+    this.signupForm = this.formBuilder.group({
+      // your existing form controls
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      artistID: ['']
+    });
+  }
 
   selectedFile: string = '';
   selectedSong: string = '';
+  signupForm: FormGroup;
+  file: File | null = null;
 
+  ngOnInit() {
+    // Retrieve the token from localStorage
+    const token = localStorage.getItem('token');
+  
+    // Check if the token is not null or undefined before setting the value
+    if (token) {
+      // Set the 'artistId' form control value
+      this.signupForm.get('artistID')!.setValue(token);
+    }
+  }
+
+  onSubmit(): void {
+
+    
+    if (this.signupForm.valid && this.file) {
+      const formData = this.signupForm.value;
+      console.log("Submit");
+      this.songService.uploadSong(this.file, formData).subscribe(
+        (data) => {
+          console.log('Song uploaded successfully:', data);
+          // Handle the response from the server if needed
+        },
+        (error) => {
+          console.error('Error uploading song:', error);
+        }
+      );
+    }
+    this.closeDialog()
+  }
 
   closeDialog() {
     this.dialogRef.close();
@@ -44,10 +83,10 @@ export class CreateSongComponent {
     const inputElement = event.target as HTMLInputElement;
     if (inputElement.files) {
       // Access the selected file(s) from the files property
-      const file = inputElement.files[0];
+      this.file = inputElement.files[0];
       // You can also display the file name or other information as needed
-      if (file) {
-        this.selectedSong = file.name
+      if (this.file) {
+        this.selectedSong = this.file.name
       }
     }
   }
