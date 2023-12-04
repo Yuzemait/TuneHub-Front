32f1975a66774c2076f6164277e92a5b496bb210
Yuzemait <jose.andres15@hotmail.com>
2023-12-03 23:56:48 -0600

    new environment

--- tunehub/src/environments/environment.ts
+++ tunehub/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'http://localhost:3000/',
+    apiUrl: 'https://tunehub-hnol.onrender.com/',
     googleClient: '1017923178541-4nv2g8d8oim8op4fpun9crtpqkuh3gur.apps.googleusercontent.com'
 };
